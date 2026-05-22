ea41ca264bd71ca50130c920680c2862c04a11a5
Bastien2004 <bastienhecquet2004@gmail.com>
2026-05-22 10:06:53 +0200

    Réalisation

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser; // <-- AJOUTER CETTE LIGNE
+use Filament\Panel; // <-- AJOUTER CETTE LIGNE
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,10 +14,19 @@ use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser // <-- MODIFIER CETTE LIGNE
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * Autoriser l'accès à Filament en production
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Autorise uniquement votre adresse email sur le VPS
+        return $this->email === 'frederic.oden.tailleur.pierre@gmail.com';
+    }
 
     /**
      * Get the attributes that should be cast.
